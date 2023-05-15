0317f0670d5563476982cf7fed13ebd6815452d4
Devalda <briandevalda45@gmail.com>
2023-05-15 15:47:45 +0700

    "check branch"

---
+++ training - storyboard/LoginScreen.swift
@@ -0,0 +1,8 @@
+//
+//  LoginScreen.swift
+//  training - storyboard
+//
+//  Created by DEVALDA on 15/05/23.
+//
+
+import Foundation
---
+++ training - storyboard/LoginScreenViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginScreenViewController.swift
+//  training - storyboard
+//
+//  Created by DEVALDA on 15/05/23.
+//
+
+import UIKit
+
+class LoginScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
